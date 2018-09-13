7663eeabcf6ae550d2170c42642758053d2cc6ce
skybase22 <bes-noppadol0147@hotmail.com>
2018-09-13 11:19:38 +0700

    hellored

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-export default class CharacterCard extends Component {
-     render(){
-              return (
-                        <div className = "card">{this.props.value}</div>         
-                     )     
-              } 
-}
+export default class CharacterCard extends Component {     
+	constructor(props){         
+		super(props)         
+		this.state = {             
+			active: false,         
+		}     
+	}          
+
+	activate = () => {         
+		if(!this.state.active){             
+			this.setState({active: true})         
+		}     
+	} 
+ 
+    render(){         
+    	let className = `card ${this.state.active ? 'activeCard': ''}`         
+    	return (             
+    		<div className={className} onClick={this.activate}>                 
+    		{this.props.value}             
+    		</div>         
+    		)     
+    	} 
+    } 
