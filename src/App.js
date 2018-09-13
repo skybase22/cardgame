d4f978546dc9c6b488eb781dae28837ca637a2f1
skybase22 <bes-noppadol0147@hotmail.com>
2018-09-13 10:44:07 +0700

    hio2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import CharacterCard from './CharacterCard'; 
+
+const word = "Hello"; 
 class App extends Component {   
   render() {     
     return (       
-      <div>         
-      <CharacterCard value="h"/>  
-      <CharacterCard value="i"/>
-      <CharacterCard value="a"/>       
+      <div>         {           
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)        
+      }       
       </div>     
       );   
+      } 
     } 
-  } 
-  export default App;
-
+export default App;
